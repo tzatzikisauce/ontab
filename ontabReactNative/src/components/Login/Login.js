71c6dc1406bf60764951b2aa819afa223148bf5e
Israel Miles <ismi7509@colorado.edu>
2020-03-08 10:58:33 -0600

    add background image

--- ontabReactNative/src/components/Login/Login.js
+++ ontabReactNative/src/components/Login/Login.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View } from 'react-native';
-
+import {
+    Image,
+    ImageBackground,
+    Text,
+    StyleSheet,
+    View
+} from 'react-native';
+import bgImage from '../../../assets/images/app_background.jpg';
 
 export default class Login extends Component {
     render() {
         return (
             <View style = { styles.container }>
+                <ImageBackground style = { styles.backgroundImage } source = {bgImage}>
 
+                </ImageBackground>
             </View>
         )
     }
@@ -16,5 +24,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#3238db'
+    },
+
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        justifyContent: "center",
+        alignItems: "center",
+        opacity: 0.7
     }
 })
